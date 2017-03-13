a7049baa2abc5c3fcf962be4b8a9f70a87d75dbb
Raghu Betina <raghu@firstdraft.com>
2017-03-13 03:54:57 +0000

    Direct association between users and recipes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ApplicationRecord
   # Direct associations
 
+  has_many   :recipes,
+             :foreign_key => "creator_id",
+             :dependent => :destroy
+
   has_many   :pantry_items,
              :foreign_key => "userid",
              :dependent => :destroy
